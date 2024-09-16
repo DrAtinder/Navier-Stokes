08643798d67d1259ff212d8265d6cc922fc40c43
Atinder Pal Singh <atinder.singh@essteyr.com>
2024-09-16 20:11:42 +0200

    delete build folder

--- build/CMakeFiles/NSsolver.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for NSsolver.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atinder/Documents/NS/Navier-Stokes
-# Build directory: /home/atinder/Documents/NS/Navier-Stokes/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Mesh")
